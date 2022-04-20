e652c3dc8722d6e5f3b2560c605a14034263238e
Serg <pseesp@mail.ru>
2022-04-20 12:36:06 +0300

    NaN2None + task7.1

--- task7pse.py
+++ task7pse.py
@@ -88,5 +88,100 @@ else:
 finally:
     print("inserted {} rows".format(curs.rowcount))
 
+# make query for task 7.1
+# Вы забираете данные с листа 'hard'. Нужно отыскать пациентов, у которых не
+# в норме два и более анализов. Вывести телефон, имя, название анализа и
+# заключение 'Повышен', 'Понижен' или 'Положительный'. Сохранить в xlsx.
+
+sql_str = """
+SELECT * FROM
+(
+SELECT 
+    --tr.id,
+    tr.phone,
+    tr.name client,
+    a.name an_name,
+    case
+        when a.simple is not null and  tr.simpl is not null and  a.simple = 'Y' and  tr.simpl = 'Y' then 'Положительный'
+        when tr.val is not null and  tr.val < a.min_value then 'Понижен' 
+        when tr.val is not null and  tr.val > a.max_value then 'Повышен' 
+        else 'Норма'
+    end  as res
+FROM
+    (SELECT 
+        t.id,
+        t.name,
+        t.phone,
+        r.pat_code,
+        r.an_code,
+        r.val,
+        r.simpl
+    FROM DE.MED_NAMES t
+    LEFT JOIN DEMIPT2.PANA_XLS r
+        ON t.id = r.pat_code
+    ORDER BY t.id
+    ) tr
+    LEFT JOIN DE.MED_AN_NAME a
+        ON tr.an_code = a.code
+WHERE tr.id in (
+--
+    SELECT 
+        resul.id
+    FROM
+    (
+    SELECT 
+        tr.id,
+        tr.phone,
+        tr.name client,
+        tr.an_code,
+        a.name an_name,
+        case
+            when a.simple is not null and  tr.simpl is not null and  a.simple = 'Y' and  tr.simpl = 'Y' then 'Положительный'
+            when tr.val is not null and  tr.val < a.min_value then 'Понижен' 
+            when tr.val is not null and  tr.val > a.max_value then 'Повышен' 
+            else 'Норма'
+        end  as res,
+        
+        case
+            when a.simple is not null and  tr.simpl is not null and  a.simple = 'Y' and  tr.simpl = 'Y' then 1
+            when tr.val is not null and  tr.val < a.min_value then 1 
+            when tr.val is not null and  tr.val > a.max_value then 1 
+            else 0
+        end  as kol
+        
+    FROM
+        (SELECT 
+            t.id,
+            t.name,
+            t.phone,
+            r.pat_code,
+            r.an_code,
+            r.val,
+            r.simpl
+        FROM DE.MED_NAMES t
+        LEFT JOIN DEMIPT2.PANA_XLS r
+            ON t.id = r.pat_code
+        ORDER BY t.id
+        ) tr
+        LEFT JOIN DE.MED_AN_NAME a
+            ON tr.an_code = a.code
+        ) resul
+    GROUP BY resul.id
+    HAVING sum(resul.kol) >= 2
+--
+    )
+)
+WHERE res <> 'Норма'
+"""
+print("\ngetting from  database...\n")
+try:
+    curs.execute(sql_str)
+except Exception as e:
+    print("Error getting data:{}".format(e))
+else:
+    df = pd.DataFrame(curs.fetchall(), columns=[x[0] for x in curs.description])
+    print(df)
+
+
 curs.close()
 conn.close()
